5dc4380398103a78749c311681e5316c7f31f118
candelafatala <137414595+candelafatala@users.noreply.github.com>
2023-08-30 16:29:30 -0300

    Carpinfo Project

---
+++ Carpinfo/src/main/java/com/carpinfo/CarpinfoApplication.java
@@ -0,0 +1,13 @@
+package com.carpinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarpinfoApplication.class, args);
+	}
+
+}
---
+++ Carpinfo/src/test/java/com/carpinfo/CarpinfoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carpinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarpinfoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
